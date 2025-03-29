bd41a2a60a73c23df0459e6fcfabf02a745fa6f3
chris01ikom <40614276-chris01ikom@users.noreply.replit.com>
2025-03-29 11:46:45 +0000

    Restored to '05110da2d2476d52afa0db301f1803fef59ee854'
    Replit-Restored-To: 05110da2d2476d52afa0db301f1803fef59ee854

--- client/src/components/nav-bar.tsx
+++ client/src/components/nav-bar.tsx
@@ -26,7 +26,7 @@ export function NavBar() {
   };
 
   const navItems: NavItem[] = [
-    { href: "/", label: "My Availability" },
+    { href: "/", label: "Set Availability" },
     { href: "/roster", label: "View Roster" },
     { href: "/account", label: "Account" },
     ...(user?.isAdmin ? [{ href: "/admin", label: "Admin" }] : []),
